331c8fbe9480ccd4f3ba5311d8a695f5c1090f0c
taoyuzhu <taoyuzhu@hulai.com>
2019-07-27 22:34:38 +0800

    7.27

--- src/components/common/Sidebar.vue
+++ src/components/common/Sidebar.vue
@@ -3,24 +3,30 @@
     <el-menu class="sidebar-el-menu" :default-active="onRoutes" :collapse="collapse" background-color="#324157"
              text-color="#bfcbd9" active-text-color="#20a0ff" unique-opened router>
       <template v-for="item in items">
+
         <template v-if="item.subs">
           <el-submenu :index="item.index" :key="item.index">
             <template slot="title">
               <i :class="item.icon"></i><span slot="title">{{ item.title }}</span>
             </template>
+
             <template v-for="subItem in item.subs">
               <el-submenu v-if="subItem.subs" :index="subItem.index" :key="subItem.index">
-                <template slot="title">{{ subItem.title }}</template>
+                <template slot="title">
+                  {{ subItem.title }}
+                </template>
                 <el-menu-item v-for="(threeItem,i) in subItem.subs" :key="i" :index="threeItem.index">
-                  {{ threeItem.title }}
+                  <i :class="threeItem.icon"></i><span slot="title"> {{ threeItem.title }}</span>
                 </el-menu-item>
               </el-submenu>
+
               <el-menu-item v-else :index="subItem.index" :key="subItem.index">
-                {{ subItem.title }}
+                <i :class="subItem.icon"></i><span slot="title">{{ subItem.title }}</span>
               </el-menu-item>
             </template>
           </el-submenu>
         </template>
+
         <template v-else>
           <el-menu-item :index="item.index" :key="item.index">
             <i :class="item.icon"></i><span slot="title">{{ item.title }}</span>
