c312684a916866e93085144695e6c2ddf40386d6
taoyuzhu <taoyuzhu@hulai.com>
2019-08-06 21:54:40 +0800

    8/6

--- src/components/page/User.vue
+++ src/components/page/User.vue
@@ -119,115 +119,12 @@
           <el-table-column
             fixed="right"
             label="操作"
-            width="100">
+            width="200">
             <template slot-scope="scope" style="float: right">
-
-              <!--              <el-popover
-                              placement="right"
-                              width="400"
-                              trigger="click">
-                              <el-form ref="form" :model="form" label-width="80px">
-                                <el-form-item label="活动名称">
-                                  <el-input v-model="form.name"></el-input>
-                                </el-form-item>
-                                <el-form-item label="活动区域">
-                                  <el-select v-model="form.region" placeholder="请选择活动区域">
-                                    <el-option label="区域一" value="shanghai"></el-option>
-                                    <el-option label="区域二" value="beijing"></el-option>
-                                  </el-select>
-                                </el-form-item>
-                                <el-form-item label="活动时间">
-                                  <el-col :span="11">
-                                    <el-date-picker type="date" placeholder="选择日期" v-model="form.date1"
-                                                    style="width: 100%;"></el-date-picker>
-                                  </el-col>
-                                  <el-col class="line" :span="2">-</el-col>
-                                  <el-col :span="11">
-                                    <el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
-                                  </el-col>
-                                </el-form-item>
-                                <el-form-item label="即时配送">
-                                  <el-switch v-model="form.delivery"></el-switch>
-                                </el-form-item>
-                                <el-form-item label="活动性质">
-                                  <el-checkbox-group v-model="form.type">
-                                    <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-                                    <el-checkbox label="地推活动" name="type"></el-checkbox>
-                                    <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-                                    <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-                                  </el-checkbox-group>
-                                </el-form-item>
-                                <el-form-item label="特殊资源">
-                                  <el-radio-group v-model="form.resource">
-                                    <el-radio label="线上品牌商赞助"></el-radio>
-                                    <el-radio label="线下场地免费"></el-radio>
-                                  </el-radio-group>
-                                </el-form-item>
-                                <el-form-item label="活动形式">
-                                  <el-input type="textarea" v-model="form.desc"></el-input>
-                                </el-form-item>
-                                <el-form-item>
-                                  <el-button type="primary" @click="onSubmit">立即创建</el-button>
-                                  <el-button>取消</el-button>
-                                </el-form-item>
-                              </el-form>
-                              <el-button slot="reference" @click="query(scope.row)" type="primary" round plain size="mini"
-                                         icon="el-icon-check">查看
-                              </el-button>
-                            </el-popover>
-
-                            <el-popover
-                              placement="right"
-                              width="400"
-                              trigger="click">
-                              <el-form ref="form" :model="form" label-width="80px">
-                                <el-form-item label="活动名称">
-                                  <el-input v-model="form.name"></el-input>
-                                </el-form-item>
-                                <el-form-item label="活动区域">
-                                  <el-select v-model="form.region" placeholder="请选择活动区域">
-                                    <el-option label="区域一" value="shanghai"></el-option>
-                                    <el-option label="区域二" value="beijing"></el-option>
-                                  </el-select>
-                                </el-form-item>
-                                <el-form-item label="活动时间">
-                                  <el-col :span="11">
-                                    <el-date-picker type="date" placeholder="选择日期" v-model="form.date1"
-                                                    style="width: 100%;"></el-date-picker>
-                                  </el-col>
-                                  <el-col class="line" :span="2">-</el-col>
-                                  <el-col :span="11">
-                                    <el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
-                                  </el-col>
-                                </el-form-item>
-                                <el-form-item label="即时配送">
-                                  <el-switch v-model="form.delivery"></el-switch>
-                                </el-form-item>
-                                <el-form-item label="活动性质">
-                                  <el-checkbox-group v-model="form.type">
-                                    <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-                                    <el-checkbox label="地推活动" name="type"></el-checkbox>
-                                    <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-                                    <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-                                  </el-checkbox-group>
-                                </el-form-item>
-                                <el-form-item label="特殊资源">
-                                  <el-radio-group v-model="form.resource">
-                                    <el-radio label="线上品牌商赞助"></el-radio>
-                                    <el-radio label="线下场地免费"></el-radio>
-                                  </el-radio-group>
-                                </el-form-item>
-                                <el-form-item label="活动形式">
-                                  <el-input type="textarea" v-model="form.desc"></el-input>
-                                </el-form-item>
-                                <el-form-item>
-                                  <el-button type="primary" @click="onSubmit">立即创建</el-button>
-                                  <el-button>取消</el-button>
-                                </el-form-item>
-                              </el-form>
-                              <el-button slot="reference" type="primary" round plain size="mini" icon="el-icon-edit">编辑</el-button>
-                            </el-popover>-->
-
+              <el-button slot="reference" @click="query(scope.row)" type="primary" round plain size="mini"
+                         icon="el-icon-check">查看
+              </el-button>
+              <el-button slot="reference" type="primary" round plain size="mini" icon="el-icon-edit">编辑</el-button>
             </template>
           </el-table-column>
         </el-table>
@@ -341,14 +238,16 @@
       //添加用户
       addUser: function () {
         let params = {
-          'roleIdList': this.form.roles,
+          'roleIdList': JSON.stringify(this.form.roles),
           'username': this.form.name,
           'status': this.form.region,
         }
         this.$http({
           url: this.$http.adornUrl('admin/sys/user/insertUser'),
           method: 'post',
-          data: ({}),
+          data: ({
+            'roleIdList': this.form.roles,
+          }),
           params: params
         }).then(({data}) => {
           if (data && data.code === 200) {
@@ -358,7 +257,8 @@
               duration: 2000,
               type: 'success',
               message: data.msg,
-            })
+            });
+            this.init();
           } else {
             this.$message.error(data.msg)
           }
